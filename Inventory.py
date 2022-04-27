eb8c5f638c5ec3ee98c0013e9d210713fc693dc7
bfreeman0812 <93546022+bfreeman0812@users.noreply.github.com>
2022-04-27 19:00:12 -0400

    Update Inventory.py

--- Inventory.py
+++ Inventory.py
@@ -5,6 +5,8 @@ from tkinter import ttk
 from tkinter import filedialog
 import random
 import tkinter.messagebox
+import pandas as pd
+import csv
 
 
 class Inventory:
@@ -12,44 +14,79 @@ class Inventory:
     def __init__(self,root):
         self.root=root
         self.root.title("Inventory System")
-        self.root.geometry("1350x800+0+0")
+        self.root.geometry("1360x810+0+0")
         self.root.configure(background='white')
 
         ################ FRAMES #####################
 
-        MainFrame=Frame(self.root, bd=5, width=1350, height=1350, bg="yellow", relief=RIDGE)
+        MainFrame=Frame(self.root, bd=5, width=1350, height=800, bg="yellow", relief=RIDGE)
         MainFrame.grid()
+        MainFrame.grid_propagate(0)
 
-        #LeftFrame=Frame(MainFrame, bd=5, width=750, height=600,padx=5, bg="light grey", relief=RIDGE)
-        #LeftFrame.pack(side=LEFT)
+        FrameMain=Frame(MainFrame, bd=5, width=1340, height=790,padx=5,bg="light grey", relief=RIDGE)
+        FrameMain.grid(row=0, column=0,sticky=NW)
+        FrameMain.grid_propagate(0)
 
-        RightFrame=Frame(MainFrame, bd=5, width=1300, height=1300,padx=5,bg="light grey", relief=RIDGE)
-        RightFrame.pack(side=RIGHT)
-  
-        RightFrame0=Frame(RightFrame, bd=2, width=1350, height=800,padx=5,bg="light grey", relief=RIDGE)
-        RightFrame0.grid(row=0, column=0)
-        RightFrame1=Frame(RightFrame, bd=2, width=1350, height=800,padx=5,bg="light grey", relief=RIDGE)
-        RightFrame1.grid(row=1, column=0)
-        RightFrame2=Frame(RightFrame, bd=2, width=1350, height=800,padx=5,bg="light grey", relief=RIDGE)
-        RightFrame2.grid(row=2, column=0)
+        TreeviewFrame=Frame(FrameMain, bd=2,width=1330, height=750,padx=5,bg="light grey", relief=RIDGE)
+        TreeviewFrame.grid(row=0, column=0,sticky=N+S+W+E)
+        TreeviewFrame.grid_propagate(0)
 
+        fr_x = ttk.Frame(TreeviewFrame)
+        fr_x.pack(side='top', fill='x')
+
+        ButtonFrame=Frame(FrameMain, bd=2, width=1325, height=33,padx=5,bg="light grey", relief=RIDGE)
+        ButtonFrame.grid(row=1, column=0, sticky=SW)
+        ButtonFrame.grid_propagate(0)
+
+        self.tree=ttk.Treeview(TreeviewFrame, height=34)
+      
+        label=Label(TreeviewFrame, text='Excel File Will Show Here')
+        label.pack()
+        label.grid_propagate(0)
+
+        treeScrollBar=ttk.Scrollbar(fr_x, orient= "horizontal", command=self.tree.xview)
+        treeScrollBar.pack(expand='yes', fill='x')
+
+        self.tree.configure(xscroll=treeScrollBar.set)
+        self.tree.pack(fill='both', expand='yes')
+
+        # self.tree.configure(xscroll=treeScrollBar.set)
+        # self.tree.pack(fill='both', expand='yes')
+        
         
   
         def openFile():
-                tf = filedialog.askopenfilename(
-                        initialdir="C:/Users/MainFrame/Desktop/", 
-                        title="Open Text file", 
-                        filetypes=(("Text Files", "*.txt"),)
-        )  
-                tf = open(tf)  # or tf = open(tf, 'r')
-                data = tf.read()
-                self.txtInventory.insert(END, data)
-                tf.close()
+                filename = filedialog.askopenfilename(
+                        title="Open a File", 
+                        filetype=(("xlxs files", ".*xlsx"),("All Files", "*.*"))
+                )
+                if filename:
+                        try:
+                                filename = r"{}".format(filename)
+                                df = pd.read_excel(filename)
+                        except ValueError:
+                                self.label.config(text="File could not be opened")
+                          # Clear all the previous data in tree
+                clear_treeview()
 
-        def clearScreen():
-            self.txtInventory.delete("1.0", "end")
+                # Add new data in Treeview widget
+                self.tree["column"] = list(df.columns)
+                self.tree["show"] = "headings"
 
+   # For Headings iterate over the columns
+                for col in self.tree["column"]:
+                        self.tree.heading(col, text=col)
+                        self.tree.column("# 1",anchor=CENTER, stretch=NO, width=100)
 
+   # Put Data in Rows
+                df_rows = df.to_numpy().tolist()
+                for row in df_rows:
+                        self.tree.insert("", "end", values=row)
+
+               
+
+                self.tree.pack()
+            
         def display(x):
                 print(x)
                 return
@@ -60,59 +97,84 @@ class Inventory:
                 root.destroy()
                 return
 
-        def iGo():
-                File_object=open("Inventory.txt", "r").read().find(self.txtSearch)
-                searchedItem= self.txtSearch.get()
-                display(searchedItem)
+        # def iGo():
+        #         File_object=open("Inventory.txt", "r").read().find(self.txtSearch)
+        #         searchedItem= self.txtSearch.get()
+        #         display(searchedItem)
 
-                return
+        #         return
 
         def openNewWindow():
             newWindow = Toplevel(root)
             newWindow.title("Add Item")
             newWindow.geometry("500x500")
             newWindow.configure(background="white")
-            Label(newWindow,
+            label=Label(newWindow,
             text ="").pack()
 
             newFrame=Frame(newWindow, bd=5, width=450, height=450, bg="yellow", relief=RIDGE)
             newFrame.grid(row=0, column=0)
 
-            self.txtSearch= Entry(newFrame, width= 29, font=('arial', 9, 'bold'))
-            self.txtSearch.grid(row=0, column=0)
+            self.searchEntry= Entry(newFrame, width= 29, font=('arial', 9, 'bold'))
+            self.searchEntry.grid(row=0, column=0)
             return
         
-        def AddItems():
-                F=open("Inventory.txt", "w+")
-                UserInput= input("Enter Item name, quantity and Bay #: ")
-                with open(F, "w+") as f:
-                    f.write(input())
+        def clear_treeview():
+                self.tree.delete(*self.tree.get_children())
+
+        def updateEntry():
+                pass
+
+        def saveFile():
+                cols = ['ID CARD','NAME','SURNAME', 'DATE']
+                path = 'read.csv'
+                excel_name = filedialog.asksaveasfilename(title='Save location',defaultextension=[('Excel','*.xlsx')],filetypes=[('Excel','*.xlsx')]) 
+                lst = []
+                with open(path, "w", newline='') as myfile:
+                        csvwriter = csv.writer(myfile, delimiter=',')
+                        for row_id in self.tree.get_children():
+                                row = self.tree.item(row_id,'values')
+                                lst.append(row)
+                        lst = list(map(list,lst))
+                        lst.insert(0,cols)
+                        for row in lst:
+                                csvwriter.writerow(row)
+
+                writer = pd.ExcelWriter(excel_name)
+                df = pd.read_csv(path)
+                df.to_excel(writer,'sheetname')
+                writer.save() 
 
 
-                return
   
    ################ RightFrame0 for Text Labels, Widget #####################
 
-        self.lblSearch=Label(RightFrame0, font=('arial', 18, 'bold'), text= "Search:" , padx=2, pady=2, bg="light grey")
-        self.lblSearch.grid(row=0, column=0, sticky=W)
-        self.txtSearch= Entry(RightFrame0, width= 29, font=('arial', 9, 'bold'))
-        self.txtSearch.grid(row=0, column=1)
+        #self.lblSearch=Label(Frame0, font=('arial', 18, 'bold'), text= "Search:" , padx=2, pady=2, bg="light grey")
+        #self.lblSearch.grid(row=0, column=0, sticky=W)
+       # self.txtSearch= Entry(Frame0, width= 29, font=('arial', 9, 'bold'))
+       # self.txtSearch.grid(row=0, column=1)
+        
   
   ################ Frames for Text Labels, Widget #####################
-        self.txtInventory= Text(RightFrame1, height= 45, width= 185, font=('arial', 9, 'bold'))
-        self.txtInventory.grid(row=0, column=0)
-
+       # self.txtInventory= Text(RightFrame1, height= 45, width= 185, font=('arial', 9, 'bold'))
+       # self.txtInventory.grid(row=0, column=0)
+      
         ################ Buttons #######################
-        self.btnAddItems= Button(RightFrame2, padx=18, pady=2, bd=2, fg="black", font=('arial', 9, 'bold'), width=9,
-                 bg="light grey", text="Add Item(s)",command= openNewWindow).grid(row=0, column=0)
-        self.btnShowInventory= Button(RightFrame2, padx=18, pady=2, bd=2, fg="black", font=('arial', 9, 'bold'), width=14,
-                 bg="light grey", text="Open Inventory File", command= openFile).grid(row=0, column=1)
-        self.btnExit= Button(RightFrame2, padx=18, pady=2, bd=2, fg="black", font=('arial', 9, 'bold'), width=9,
-                 bg="light grey", text="Exit",command=iExit).grid(row=0, column=2)
-        self.btnClear= Button(RightFrame2, padx=18, pady=2, bd=2, fg="black", font=('arial', 9, 'bold'), width=9,
-                 bg="light grey", text="Clear Screen",command=clearScreen).grid(row=0, column=3)
-        self.btnGo= Button(RightFrame0, padx=2, pady=2, bd=2, fg="black", font=('arial', 9, 'bold'), width=9,
-                 bg="light grey", text="Go",command=iGo).grid(row=0, column=2)
+        self.btnUpdate= Button(ButtonFrame, padx=18, pady=2, bd=2, fg="black", font=('arial', 9, 'bold'), width=9,
+                 bg="light grey", text="Update Entry", command= updateEntry).grid(row=1, column=0)
+        self.btnSave= Button(ButtonFrame, padx=18, pady=2, bd=2, fg="black", font=('arial', 9, 'bold'), width=9,
+                 bg="light grey", text="Save",command= saveFile).grid(row=1, column=1)
+        self.btnAddItems= Button(ButtonFrame, padx=18, pady=2, bd=2, fg="black", font=('arial', 9, 'bold'), width=9,
+                 bg="light grey", text="Add Item(s)",command= openNewWindow).grid(row=1, column=2)
+        self.btnShowInventory= Button(ButtonFrame, padx=18, pady=2, bd=2, fg="black", font=('arial', 9, 'bold'), width=14,
+                 bg="light grey", text="Open Inventory File", command= openFile).grid(row=1, column=3)
+        self.btnExit= Button(ButtonFrame, padx=18, pady=2, bd=2, fg="black", font=('arial', 9, 'bold'), width=9,
+                 bg="light grey", text="Exit",command=iExit).grid(row=1, column=4)
+        self.btnClear= Button(ButtonFrame, padx=18, pady=2, bd=2, fg="black", font=('arial', 9, 'bold'), width=9,
+                 bg="light grey", text="Clear Screen",command=clear_treeview).grid(row=1, column=5)
+      
+       # self.btnGo= Button(Frame0, padx=2, pady=2, bd=2, fg="black", font=('arial', 9, 'bold'), width=9,
+        #         bg="light grey", text="Go",command=iGo).grid(row=0, column=2)
 
 
 
